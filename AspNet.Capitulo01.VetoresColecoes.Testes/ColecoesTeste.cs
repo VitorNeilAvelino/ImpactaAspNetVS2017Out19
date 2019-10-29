ecf23e5dab751312fb1b6f55f227e2d63c816124
Vitor Neil Avelino <avelino.vitor@gmail.com>
2019-10-28 23:58:37 -0200

    Vetores e coleções

---
+++ AspNet.Capitulo01.VetoresColecoes.Testes/ColecoesTeste.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AspNet.Capitulo01.VetoresColecoes.Testes
+{
+    [TestClass]
+    public class ColecoesTeste
+    {
+        [TestMethod]
+        public void ListTeste()
+        {
+            var inteiros = new List<int>(1000) { 16, 3, 34, 2, -81};
+            inteiros.Add(15);
+            inteiros[0] = 23;
+            //inteiros[10] = 2;
+
+            var maisInteiros = new List<int> { 4, 2, 3 };
+
+            inteiros.AddRange(maisInteiros);
+
+            inteiros.Insert(0, 29);
+
+            inteiros.Remove(2);
+
+            inteiros.RemoveAll(x => x == 2);
+
+            inteiros.RemoveAt(4);
+
+            inteiros.Sort();
+
+            var primeiro = inteiros[0];
+            primeiro = inteiros.First();
+
+            var ultimo = inteiros.Last();
+            ultimo = inteiros[inteiros.Count - 1];
+
+            foreach (var inteiro in inteiros)
+            {
+                Console.WriteLine($"{inteiros.IndexOf(inteiro)}: {inteiro}");
+            }
+        }
+    }
+}
