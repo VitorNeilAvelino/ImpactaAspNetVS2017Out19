7805edffed02040ed92c2314f36bdbe6bebe9843
Vitor Neil Avelino <avelino.vitor@gmail.com>
2019-11-13 23:44:07 -0200

    ViagensOnline.Mvc

--- ViagensOnline.Repositorios.SqlServerTests/ViagensOnlineDbContextTests.cs
+++ ViagensOnline.Repositorios.SqlServerTests/ViagensOnlineDbContextTests.cs
@@ -27,5 +27,35 @@ namespace ViagensOnline.Repositorios.SqlServer.Tests
 
             db.SaveChanges();
         }
+
+        [TestMethod]
+        public void AtualizarTeste()
+        {
+            //db.Destinos.Where();
+            var destino = db.Destinos.Find(1);
+
+            destino.NomeImagem = "guinness.png";
+
+            db.SaveChanges();
+
+            //destino = db.Destinos.Where(d => d.Id == 1).SingleOrDefault();
+            destino = db.Destinos.SingleOrDefault(d => d.Id == 1);
+
+            Assert.AreEqual(destino.NomeImagem, "guinness.png");
+        }
+
+        [TestMethod]
+        public void ExcluirTeste()
+        {
+            var destino = db.Destinos.Find(1);
+
+            db.Destinos.Remove(destino);
+
+            db.SaveChanges();
+
+            destino = db.Destinos.Find(1);
+
+            Assert.IsNull(destino);
+        }
     }
 }
