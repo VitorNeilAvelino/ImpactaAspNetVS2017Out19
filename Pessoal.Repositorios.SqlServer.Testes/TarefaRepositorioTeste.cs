89fafd4078c8b2f23742e7c83fadc5e6ad8d5f7a
Vitor Neil Avelino <avelino.vitor@gmail.com>
2019-11-19 23:27:26 -0200

    Testes para os repositórios

--- Pessoal.Repositorios.SqlServer.Testes/TarefaRepositorioTeste.cs
+++ Pessoal.Repositorios.SqlServer.Testes/TarefaRepositorioTeste.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pessoal.Dominio.Entidades;
 
 namespace Pessoal.Repositorios.SqlServer.Testes
 {
@@ -11,16 +12,66 @@ namespace Pessoal.Repositorios.SqlServer.Testes
         public TarefaRepositorioTeste()
         {
             var config = new ConfigurationBuilder()
-                .Add()
+                .AddJsonFile("appsettings.json")
+                .Build();
                 
 
-            repositorio = new TarefaRepositorio();
+            repositorio = new TarefaRepositorio(
+                config.GetConnectionString("pessoalSqlServer"));
         }
 
         [TestMethod]
         public void InserirTeste()
         {
+            var tarefa = new Tarefa();
+            tarefa.Nome = "Limpar a casa";
+            tarefa.Observacao = "Passar pano na casa toda";
+            tarefa.Prioridade = Prioridade.Alta;
+            tarefa.Concluida = true;
 
+            tarefa.Id = repositorio.Inserir(tarefa);
+
+            Assert.IsTrue(tarefa.Id > 0);
+        }
+
+        [TestMethod]
+        public void SelecionarTodosTeste()
+        {
+            Assert.IsTrue(repositorio.Selecionar().Count > 0);
+        }
+
+        [TestMethod]
+        public void SelecionarPorIdTeste()
+        {
+            //var tarefa = repositorio.Selecionar(1);
+
+            Assert.IsNotNull(repositorio.Selecionar(1));
+            Assert.IsNull(repositorio.Selecionar(0));
+        }
+
+        [TestMethod]
+        public void AtualizarTeste()
+        {
+            var tarefa = new Tarefa();
+            tarefa.Id = 1;
+            tarefa.Nome = "Limpar a casaaaaaa";
+            tarefa.Observacao = "Passar pano na casa todaaaaa";
+            tarefa.Prioridade = Prioridade.Baixa;
+            tarefa.Concluida = false;
+            repositorio.Atualizar(tarefa);
+
+            tarefa = repositorio.Selecionar(1);
+
+            Assert.IsTrue(tarefa.Id == 1);
+            Assert.AreEqual(tarefa.Prioridade, Prioridade.Baixa);
+        }
+
+        [TestMethod]
+        public void ExcluirTeste()
+        {
+            repositorio.Excluir(1);
+
+            Assert.IsNull(repositorio.Selecionar(1));
         }
     }
 }
