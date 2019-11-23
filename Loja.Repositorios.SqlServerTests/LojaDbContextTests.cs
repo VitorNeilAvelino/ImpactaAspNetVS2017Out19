3720eb03719dcf00c3ac675a2f816b902fb07bd1
Vitor Neil Avelino <avelino.vitor@gmail.com>
2019-11-22 23:49:44 -0200

    Migrations

--- Loja.Repositorios.SqlServerTests/LojaDbContextTests.cs
+++ Loja.Repositorios.SqlServerTests/LojaDbContextTests.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using Loja.Dominio;
+using System.Data.Entity;
 
 namespace Loja.Repositorios.SqlServer.Tests
 {
@@ -34,6 +35,60 @@ namespace Loja.Repositorios.SqlServer.Tests
 
             db.Categorias.Add(categoria);
             db.SaveChanges();
+        }
+
+        [TestMethod]
+        public void InserirProdutoTeste()
+        {
+            var produto = new Produto();
+            produto.Nome = "Grampeador";
+            produto.Preco = 22.15m;
+            produto.Estoque = 15;
+            produto.Categoria = new Categoria { Nome = "Papelaria" };
+            //produto.Categoria = db.Categorias.Find(1);
+
+            db.Produtos.Add(produto);
+            db.SaveChanges();
+        }
+
+        [TestMethod]
+        public void LazyLoadTeste()
+        {
+            var produto = db.Produtos
+                .Where(p => p.Nome == "Celular")
+                .First();
+
+            Console.WriteLine(produto.Categoria.Nome);
+        }
+
+        [TestMethod]
+        public void IncludeTeste()
+        {
+            var produto = db.Produtos
+                .Include(p => p.Categoria)
+                .Single(p => p.Id == 1);
+
+            Console.WriteLine(produto.Categoria.Nome);
+        }
+
+        [TestMethod]
+        [DataRow(1)]
+        public void QueryableTeste(int estoque)
+        {
+            var query = db.Produtos.Where(p => p.Preco > 10);
+
+            if (estoque > 0)
+            {
+                query = query.Where(p => p.Estoque > estoque);
+            }
+
+            query.OrderBy(p => p.Nome);
+
+            var primeiro = query.FirstOrDefault();
+            var ultimo = query.AsEnumerable().Last();
+            //var unico = query.Single(p => p.Id == 1);
+            var unico = query.SingleOrDefault();
+            var todos = query.ToList();
         }
     }
 }
