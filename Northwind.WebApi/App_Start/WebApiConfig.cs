1c7d3f5e379cd3bdaafafebb44cec62b70bd7279
Vitor Neil Avelino <avelino.vitor@gmail.com>
2019-11-24 18:06:51 -0300

    Web API

---
+++ Northwind.WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Northwind.WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Serviços e configuração da API da Web
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            // Rotas da API da Web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
